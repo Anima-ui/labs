d48f2ba2025766bde87e9eecfd4d6a5f251f46ad
Anima-ui <remlrk@mail.ru>
2025-02-25 17:24:25 +0300

    Add files via upload

---
+++ lab9/funcs.h
@@ -0,0 +1,14 @@
+#pragma once
+void getValidFilename(char* filename, size_t size);
+
+void validFileInput(FILE* file);
+
+void countMultiplesOf(FILE* file);
+
+void fileShift(FILE* file);
+
+int someValidIntInput(const char* str);
+
+void printFileContent(FILE* file);
+
+void repeatReplace(FILE* file);
